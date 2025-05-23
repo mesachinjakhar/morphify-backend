574f7b658f142e93dddbda102defb0d7d75c0551
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-05-23 16:22:15 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+import errorHandler from "./middlewares/error-handler.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+app.use(errorHandler);
+
+export default app;
