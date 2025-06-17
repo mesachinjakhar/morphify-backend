f70a2b84baef60ed5eeba178587c766c88763df1
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-06-17 19:31:08 +0530

    return added

--- src/routes/rewardsRoutes.ts
+++ src/routes/rewardsRoutes.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express";
-import { PhotoHandler } from "../handlers/photoHandler";
 
 const router = Router();
 
 router.get("/verify", (req: Request, res: Response) => {
   console.log("recieved paramets: ", req.params);
   res.status(200);
+  return;
 });
 
 export default router;
