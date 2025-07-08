8e91f662a8b0359aee40d216def95a24bbad0ed1
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-07-08 05:36:20 +0530

    version added

--- src/services/providers/tencentarc/gfpgan.ts
+++ src/services/providers/tencentarc/gfpgan.ts
@@ -41,6 +41,8 @@ export class GfpganProvider implements IProvider {
 
     // create prediction
     const prediction = await this.replicate.predictions.create({
+      version:
+        "0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c",
       model: "tencentarc/gfpgan",
       input: {
         img: imageUrl,
